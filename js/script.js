b871fdb62b605d2c67d0b25666d936a0ae3c7c6b
Paul Davis <pdavis@PaulDavisLaptop.localdomain>
2020-04-23 11:56:36 -0700

    control with year works for get teams

--- js/script.js
+++ js/script.js
@@ -3,19 +3,22 @@
 //when you click the button the text in div 1 changes
 var btn1 = document.getElementById("btn").addEventListener("click",getTeams);
 
-function changeText(){
-    
-    alert("You clicked me!");
-}
-
-
-
 function getTeams (){
 //set the html contents of index.html dynamically
+    
+    //function expression to assing data HTML div element to variable
     var data = document.getElementById("data");
-    //data.innerHTML="Trains";
 
-    return fetch('http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code=%27mlb%27&all_star_sw=%27N%27&sort_order=name_asc&season=%272017%27')
+    //function expression to assign RosterYear form input element to variable
+    var inptRosterYr = document.getElementById("RosterYear");
+
+    //function expression to assign user input year to RosterYear
+        //TODO validate roster year input date and prompt the user to enter a date in the valid range for MLB Data
+            // - probably need to use an event lister on the Go button too
+    let RosterYear = inptRosterYr.value;
+    //alert(RosterYear);
+
+    return fetch('http://lookup-service-prod.mlb.com/json/named.team_all_season.bam?sport_code=%27mlb%27&all_star_sw=%27N%27&sort_order=name_asc&season=%27'+RosterYear+'%27')
         .then(function(response){            
             return response.json();
         })
@@ -39,5 +42,4 @@ function getTeams (){
             //return response.data
 
         })
-
     }
